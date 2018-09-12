afb1ebbc66f092d32e58e137cd7f6794aba3abd1
Aloisio Eduardo Vieira <alvieira@br.ibm.com>
2018-09-11 21:34:15 -0300

    data model updates

---
+++ src/test/java/com/jhipcon/web/rest/BlogResourceIntTest.java
@@ -0,0 +1,289 @@
+package com.jhipcon.web.rest;
+
+import com.jhipcon.BlogApp;
+
+import com.jhipcon.domain.Blog;
+import com.jhipcon.repository.BlogRepository;
+import com.jhipcon.web.rest.errors.ExceptionTranslator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Base64Utils;
+
+import java.util.List;
+
+
+import static com.jhipcon.web.rest.TestUtil.createFormattingConversionService;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Test class for the BlogResource REST controller.
+ *
+ * @see BlogResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BlogApp.class)
+public class BlogResourceIntTest {
+
+    private static final String DEFAULT_TITLE = "AAAAAAAAAA";
+    private static final String UPDATED_TITLE = "BBBBBBBBBB";
+
+    private static final String DEFAULT_AUTHOR = "AAAAAAAAAA";
+    private static final String UPDATED_AUTHOR = "BBBBBBBBBB";
+
+    private static final byte[] DEFAULT_POST = TestUtil.createByteArray(1, "0");
+    private static final byte[] UPDATED_POST = TestUtil.createByteArray(2, "1");
+    private static final String DEFAULT_POST_CONTENT_TYPE = "image/jpg";
+    private static final String UPDATED_POST_CONTENT_TYPE = "image/png";
+
+    @Autowired
+    private BlogRepository blogRepository;
+
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    private MockMvc restBlogMockMvc;
+
+    private Blog blog;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        final BlogResource blogResource = new BlogResource(blogRepository);
+        this.restBlogMockMvc = MockMvcBuilders.standaloneSetup(blogResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setConversionService(createFormattingConversionService())
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Blog createEntity() {
+        Blog blog = new Blog()
+            .title(DEFAULT_TITLE)
+            .author(DEFAULT_AUTHOR)
+            .post(DEFAULT_POST)
+            .postContentType(DEFAULT_POST_CONTENT_TYPE);
+        return blog;
+    }
+
+    @Before
+    public void initTest() {
+        blogRepository.deleteAll();
+        blog = createEntity();
+    }
+
+    @Test
+    public void createBlog() throws Exception {
+        int databaseSizeBeforeCreate = blogRepository.findAll().size();
+
+        // Create the Blog
+        restBlogMockMvc.perform(post("/api/blogs")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(blog)))
+            .andExpect(status().isCreated());
+
+        // Validate the Blog in the database
+        List<Blog> blogList = blogRepository.findAll();
+        assertThat(blogList).hasSize(databaseSizeBeforeCreate + 1);
+        Blog testBlog = blogList.get(blogList.size() - 1);
+        assertThat(testBlog.getTitle()).isEqualTo(DEFAULT_TITLE);
+        assertThat(testBlog.getAuthor()).isEqualTo(DEFAULT_AUTHOR);
+        assertThat(testBlog.getPost()).isEqualTo(DEFAULT_POST);
+        assertThat(testBlog.getPostContentType()).isEqualTo(DEFAULT_POST_CONTENT_TYPE);
+    }
+
+    @Test
+    public void createBlogWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = blogRepository.findAll().size();
+
+        // Create the Blog with an existing ID
+        blog.setId("existing_id");
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restBlogMockMvc.perform(post("/api/blogs")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(blog)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Blog in the database
+        List<Blog> blogList = blogRepository.findAll();
+        assertThat(blogList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    public void checkTitleIsRequired() throws Exception {
+        int databaseSizeBeforeTest = blogRepository.findAll().size();
+        // set the field null
+        blog.setTitle(null);
+
+        // Create the Blog, which fails.
+
+        restBlogMockMvc.perform(post("/api/blogs")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(blog)))
+            .andExpect(status().isBadRequest());
+
+        List<Blog> blogList = blogRepository.findAll();
+        assertThat(blogList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    public void checkAuthorIsRequired() throws Exception {
+        int databaseSizeBeforeTest = blogRepository.findAll().size();
+        // set the field null
+        blog.setAuthor(null);
+
+        // Create the Blog, which fails.
+
+        restBlogMockMvc.perform(post("/api/blogs")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(blog)))
+            .andExpect(status().isBadRequest());
+
+        List<Blog> blogList = blogRepository.findAll();
+        assertThat(blogList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    public void getAllBlogs() throws Exception {
+        // Initialize the database
+        blogRepository.save(blog);
+
+        // Get all the blogList
+        restBlogMockMvc.perform(get("/api/blogs?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(blog.getId())))
+            .andExpect(jsonPath("$.[*].title").value(hasItem(DEFAULT_TITLE.toString())))
+            .andExpect(jsonPath("$.[*].author").value(hasItem(DEFAULT_AUTHOR.toString())))
+            .andExpect(jsonPath("$.[*].postContentType").value(hasItem(DEFAULT_POST_CONTENT_TYPE)))
+            .andExpect(jsonPath("$.[*].post").value(hasItem(Base64Utils.encodeToString(DEFAULT_POST))));
+    }
+    
+
+    @Test
+    public void getBlog() throws Exception {
+        // Initialize the database
+        blogRepository.save(blog);
+
+        // Get the blog
+        restBlogMockMvc.perform(get("/api/blogs/{id}", blog.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(blog.getId()))
+            .andExpect(jsonPath("$.title").value(DEFAULT_TITLE.toString()))
+            .andExpect(jsonPath("$.author").value(DEFAULT_AUTHOR.toString()))
+            .andExpect(jsonPath("$.postContentType").value(DEFAULT_POST_CONTENT_TYPE))
+            .andExpect(jsonPath("$.post").value(Base64Utils.encodeToString(DEFAULT_POST)));
+    }
+    @Test
+    public void getNonExistingBlog() throws Exception {
+        // Get the blog
+        restBlogMockMvc.perform(get("/api/blogs/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void updateBlog() throws Exception {
+        // Initialize the database
+        blogRepository.save(blog);
+
+        int databaseSizeBeforeUpdate = blogRepository.findAll().size();
+
+        // Update the blog
+        Blog updatedBlog = blogRepository.findById(blog.getId()).get();
+        updatedBlog
+            .title(UPDATED_TITLE)
+            .author(UPDATED_AUTHOR)
+            .post(UPDATED_POST)
+            .postContentType(UPDATED_POST_CONTENT_TYPE);
+
+        restBlogMockMvc.perform(put("/api/blogs")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedBlog)))
+            .andExpect(status().isOk());
+
+        // Validate the Blog in the database
+        List<Blog> blogList = blogRepository.findAll();
+        assertThat(blogList).hasSize(databaseSizeBeforeUpdate);
+        Blog testBlog = blogList.get(blogList.size() - 1);
+        assertThat(testBlog.getTitle()).isEqualTo(UPDATED_TITLE);
+        assertThat(testBlog.getAuthor()).isEqualTo(UPDATED_AUTHOR);
+        assertThat(testBlog.getPost()).isEqualTo(UPDATED_POST);
+        assertThat(testBlog.getPostContentType()).isEqualTo(UPDATED_POST_CONTENT_TYPE);
+    }
+
+    @Test
+    public void updateNonExistingBlog() throws Exception {
+        int databaseSizeBeforeUpdate = blogRepository.findAll().size();
+
+        // Create the Blog
+
+        // If the entity doesn't have an ID, it will be created instead of just being updated
+        restBlogMockMvc.perform(put("/api/blogs")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(blog)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Blog in the database
+        List<Blog> blogList = blogRepository.findAll();
+        assertThat(blogList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    public void deleteBlog() throws Exception {
+        // Initialize the database
+        blogRepository.save(blog);
+
+        int databaseSizeBeforeDelete = blogRepository.findAll().size();
+
+        // Get the blog
+        restBlogMockMvc.perform(delete("/api/blogs/{id}", blog.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate the database is empty
+        List<Blog> blogList = blogRepository.findAll();
+        assertThat(blogList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Blog.class);
+        Blog blog1 = new Blog();
+        blog1.setId("id1");
+        Blog blog2 = new Blog();
+        blog2.setId(blog1.getId());
+        assertThat(blog1).isEqualTo(blog2);
+        blog2.setId("id2");
+        assertThat(blog1).isNotEqualTo(blog2);
+        blog1.setId(null);
+        assertThat(blog1).isNotEqualTo(blog2);
+    }
+}
